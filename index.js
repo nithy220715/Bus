50262daa7dd5e7891ba8234823b7d8e9819ea7a1
nithy220715 <nithy220715@gmail.com>
2026-05-31 19:15:00 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-
-import { View, Text } from 'react-native';
-export default function Home(){
- return <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-   <Text>🚍 My Bus</Text>
-   <Text>பேருந்து நேரங்கள்</Text>
- </View>
-}
