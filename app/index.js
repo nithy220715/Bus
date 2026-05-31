ab4b08bcb2e696a70dc9caab5f9e4d4b8c0f5f5c
nithy220715 <nithy220715@gmail.com>
2026-05-31 15:44:41 +0530

    Add files via upload

--- app/index.js
+++ app/index.js
@@ -1,1 +1,8 @@
-tew
+
+import { View, Text } from 'react-native';
+export default function Home(){
+ return <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+   <Text>🚍 My Bus</Text>
+   <Text>பேருந்து நேரங்கள்</Text>
+ </View>
+}
